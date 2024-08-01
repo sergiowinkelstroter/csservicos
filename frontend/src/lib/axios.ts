c79e3618eb8ffe4b009b16fd56e44de2760a478a
Sergio Winkelstroter <winksousa0@gmail.com>
2024-08-01 16:12:43 -0300

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "@/utils/cookies";
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:4002",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4002",
   headers: {
     "Content-Type": "application/json",
   },
